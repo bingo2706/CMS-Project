5b677679d8c3a1185c7ebe1471e91a129142b6a5
Do <thanh.do@capgemini.com>
2022-12-29 13:31:54 +0700

    refactor code and structure

--- cmsBackend/src/server.js
+++ cmsBackend/src/server.js
@@ -1,14 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import viewEngine from "./config/viewEngine";
-import initwebRoutes from "./route/web";
-import connectDB from "./config/connectDB";
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from './config/viewEngine';
+import initwebRoutes from './route/web';
+import connectDB from './config/connectDB';
 require('dotenv').config();
 
 let app = express();
 
 app.use(function (req, res, next) {
-
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -23,11 +22,13 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
-    next();
+    setTimeout(() => {
+        next();
+    }, 300);
 });
 
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 viewEngine(app);
 initwebRoutes(app);
@@ -37,5 +38,5 @@ connectDB();
 let port = process.env.PORT || 6969;
 
 app.listen(port, () => {
-    console.log("Backend Nodejs is running on the port : " + port)
+    console.log('Backend Nodejs is running on the port : ' + port);
 });
